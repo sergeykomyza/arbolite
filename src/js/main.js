6d38d01061fee34d8a6288b2856ffc10417f93ee
Сергей Комыза <s.komiza@yandex.ru>
2026-06-07 14:44:43 +0300

    fix bugs

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,7 @@ const headerLogic = () => {
     const menu = document.querySelector('.menu')
     const gamburger = document.querySelector('.header__burger')
 
-    $('.js-scroll').click(function () {
+    $('a.js-scroll').click(function () {
         var scroll_elem = $(this).attr('href') || $(this).attr('data-src');
         $('html, body').animate({
             scrollTop: $(scroll_elem).offset().top
@@ -13,6 +13,12 @@ const headerLogic = () => {
             gamburger.classList.toggle('is-open')
             menu.classList.toggle('is-active')
         }
+    });
+    $('button.js-scroll').click(function () {
+        var scroll_elem = $(this).attr('href') || $(this).attr('data-src');
+        $('html, body').animate({
+            scrollTop: $(scroll_elem).offset().top
+        }, 1000);
     });
 
     const header = document.querySelector('.header')
@@ -32,32 +38,32 @@ const headerLogic = () => {
 }
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ МАСКА ДЛЯ ИНПУТОВ (https://github.com/RobinHerbots/Inputmask)
-const inputMask = () => {
-    $(".js-maskPhone").inputmask({
-        mask: "+7 (999) 999-99-99",
-        clearIncomplete: true
-    });
-    $('.email').inputmask({
-        mask: "*{1,20}[.*{1,20}]@*{1,20}.*{2,4}",
-        clearIncomplete: true
-        //     greedy: false,
-        //     onBeforePaste: function (pastedValue, opts) {
-        //         pastedValue = pastedValue.toLowerCase();
-        //         return pastedValue.replace("mailto:", "");
-        //     },
-        //     definitions: {
-        //         '*': {
-        //             validator: "[0-9A-Za-z-а-я-]",
-        //             casing: "lower"
-        //         }
-        //     }
-    });
-    $(".js-maskDate").inputmask({
-        mask: "99/99/9999",
-        clearIncomplete: true,
-        'placeholder': 'dd/mm/yyyy'
-    });
-}
+// const inputMask = () => {
+//     $(".js-maskPhone").inputmask({
+//         mask: "+7 (999) 999-99-99",
+//         clearIncomplete: true
+//     });
+//     $('.email').inputmask({
+//         mask: "*{1,20}[.*{1,20}]@*{1,20}.*{2,4}",
+//         clearIncomplete: true
+//         //     greedy: false,
+//         //     onBeforePaste: function (pastedValue, opts) {
+//         //         pastedValue = pastedValue.toLowerCase();
+//         //         return pastedValue.replace("mailto:", "");
+//         //     },
+//         //     definitions: {
+//         //         '*': {
+//         //             validator: "[0-9A-Za-z-а-я-]",
+//         //             casing: "lower"
+//         //         }
+//         //     }
+//     });
+//     $(".js-maskDate").inputmask({
+//         mask: "99/99/9999",
+//         clearIncomplete: true,
+//         'placeholder': 'dd/mm/yyyy'
+//     });
+// }
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ СЛАЙДЕР SWIPER (https://swiperjs.com/get-started) 
 const sliders = () => {
@@ -335,52 +341,46 @@ const quiz = ()=> {
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ КАРТА, ОТЛОЖЕННАЯ ЗАГРУЗКА (ЧТОБЫ УЛУЧШИТЬ ПОКАЗАТЕЛИ - PageSpeed Insights)
 const map = () => {
 
-    setTimeout(function () {
-        var headID = document.getElementsByTagName("body")[0];
-        var newScript = document.createElement('script');
-        newScript.type = 'text/javascript';
-        newScript.src = 'https://api-maps.yandex.ru/2.1/?lang=ru_RU';
-        headID.appendChild(newScript);
-    }, 1000);
-    setTimeout(function () {
-        var myMap = new ymaps.Map("map", {
-            center: [55.427882, 37.550109],
-            zoom: 15,
-            controls: ['smallMapDefaultSet']
-        }, {
-            searchControlProvider: 'yandex#search'
-        });
+    setTimeout(() => {
+        const script = document.createElement('script');
 
-        myGeoObject = new ymaps.GeoObject({
-            geometry: {
-                type: "Point"
-            },
-        });
-        myMap.geoObjects
-            .add(myGeoObject)
-            .add(new ymaps.Placemark([55.427882, 37.550109], {
-                balloonContent: '<strong>Комсомольская улица, 1, Подольск, Московская область, 142100</strong>',
-                iconCaption: 'Комсомольская улица, 1'
-            }, {
-                preset: 'islands#blueCircleDotIconWithCaption',
-                iconCaptionMaxWidth: '200'
-            }));
+        script.src = 'https://api-maps.yandex.ru/2.1/?lang=ru_RU';
 
-        myMap.setType('yandex#publicMap');
+        script.onload = () => {
+            ymaps.ready(initMap);
+        };
 
-        myMap.behaviors.disable('scrollZoom');
-        //на мобильных устройствах... (проверяем по userAgent браузера)
-        if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-            //... отключаем перетаскивание карты
-            myMap.behaviors.disable('drag');
-        }
+        document.body.appendChild(script);
     }, 2000);
+
+    function initMap() {
+        const myMap = new ymaps.Map('map', {
+            center: [48.570612, 39.341628],
+            zoom: 16
+        });
+
+        const myPlacemark = new ymaps.Placemark(
+            [48.570612, 39.341628],
+            {
+                hintContent: 'г. Луганск, кв. Лиховида 1',
+                balloonContent: 'г. Луганск, кв. Лиховида 1'
+            },
+            {
+                iconLayout: 'default#image',
+                iconImageHref: 'img/logo_sign.png',
+                iconImageSize: [40, 45],
+                iconImageOffset: [-5, -38]
+            }
+        );
+
+        myMap.geoObjects.add(myPlacemark);
+    }
 
 }
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ INIT
 headerLogic()
-inputMask()
+// inputMask()
 sliders()
 tabs()
 // window.addEventListener('scroll', tabs)
